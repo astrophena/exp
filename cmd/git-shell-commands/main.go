2c090f16e66b73caaf3dcf210b24fad560a9dab5
Ilya Mateyko <me@astrophena.name>
2021-01-28 14:27:24 +0000

    cmd/git-shell-commands: implement the remaining commands

--- cmd/git-shell-commands/main.go
+++ cmd/git-shell-commands/main.go
@@ -5,23 +5,57 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
+	"log"
 	"os"
+	"os/exec"
 	"path/filepath"
+	"strings"
+
+	"github.com/fatih/color"
 )
 
 var commands = map[string]func(){
+	"new":                  newRepo,
 	"no-interactive-login": noInteractiveLogin,
+	"ls":                   ls,
 }
 
 func main() {
+	log.SetFlags(0)
+	color.NoColor = false
+
 	callname := filepath.Base(os.Args[0])
 
-	cmd, ok := commands[callname]
-	if !ok {
-		fmt.Fprintf(os.Stderr, "%s: no such command\n", callname)
+	cmd, exists := commands[callname]
+	if !exists {
+		fmt.Fprintf(os.Stderr, "%s: no such command.\n", callname)
 		os.Exit(127)
 	}
 	cmd()
+}
+
+func newRepo() {
+	if len(os.Args) < 2 {
+		fmt.Fprintf(os.Stderr, "Repo name is required.\n")
+		os.Exit(1)
+	}
+	repo := os.Args[1]
+
+	if !strings.HasSuffix(repo, ".git") {
+		repo = repo + ".git"
+	}
+
+	homedir, err := os.UserHomeDir()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	repopath := filepath.Join(homedir, repo)
+	log.Printf("Creating %s", repo)
+	if err := exec.Command("git", "init", "--bare", repopath).Run(); err != nil {
+		log.Fatal(err)
+	}
 }
 
 func noInteractiveLogin() {
@@ -29,4 +63,34 @@ func noInteractiveLogin() {
 provide interactive shell access.
 `)
 	os.Exit(128)
+}
+
+func ls() {
+	homedir, err := os.UserHomeDir()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	host, err := os.Hostname()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	repos, err := filepath.Glob(filepath.Join(homedir, "*.git"))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	color.Yellow("Repos on %s:", host)
+	for _, repo := range repos {
+		desc, err := ioutil.ReadFile(filepath.Join(repo, "description"))
+		if err != nil {
+			if !os.IsNotExist(err) {
+				log.Fatal(err)
+			}
+			desc = []byte("No description.")
+		}
+
+		log.Printf("%s: %s", color.MagentaString(filepath.Base(repo)), desc)
+	}
 }
--- go.mod
+++ go.mod
@@ -4,5 +4,6 @@ go 1.15
 
 require (
 	github.com/PuerkitoBio/goquery v1.6.1
+	github.com/fatih/color v1.10.0
 	github.com/senseyeio/duration v0.0.0-20180430131211-7c2a214ada46
 )
