095dbed6b52576922ce65e23291176c4f2b45d2d
Ilya Mateyko <me@astrophena.name>
2021-10-24 03:27:18 +0300

    pygo, python: various updates

--- pygo/lib.go
+++ pygo/lib.go
@@ -1,7 +1,11 @@
 package main
 
 import "C"
-import "runtime"
+import (
+	"runtime"
+
+	"git.astrophena.name/exp/watchtime"
+)
 
 //export Add
 func Add(a, b int) int {
@@ -11,6 +15,16 @@ func Add(a, b int) int {
 //export Version
 func Version() *C.char {
 	return C.CString(runtime.Version())
+}
+
+//export WatchTime
+func WatchTime(id *C.char) *C.char {
+	goId := C.GoString(id)
+	t, err := watchtime.Fetch(goId)
+	if err != nil {
+		return C.CString(err.Error())
+	}
+	return C.CString(t.String())
 }
 
 func main() {}
