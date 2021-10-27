e78287f9ab65808fe5ed637cd49895f28e638071
Ilya Mateyko <me@astrophena.name>
2021-10-27 21:38:49 +0300

    astroflask: add /ua endpoint

--- astroflask/app.py
+++ astroflask/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, make_response
 import os
 
 app = Flask(__name__)
@@ -10,6 +10,12 @@ def index():
 @app.route('/greet', methods=['POST'])
 def greet():
     return render_template('greet.html', name=request.form.get('name', 'world'))
+
+@app.route('/ua', methods=['GET'])
+def ua():
+    response = make_response(request.headers.get('User-Agent'), 200)
+    response.mimetype = 'text/plain'
+    return response
 
 @app.route('/sysinfo', methods=['GET'])
 def sysinfo():
