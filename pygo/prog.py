8d4c5e348debcac77a26f11a38eebeb7cc748a80
Ilya Mateyko <me@astrophena.name>
2021-11-15 23:16:51 +0300

    pygo: fix watch time bug

--- pygo/prog.py
+++ pygo/prog.py
@@ -6,6 +6,7 @@ print(f"Add: {lib.Add(1, 2)}")
 lib.Version.restype = ctypes.c_char_p
 print(f"Version: {lib.Version()}")
 
+lib.WatchTime.argtypes = [ctypes.c_char_p]
 lib.WatchTime.restype = ctypes.c_char_p
-videoID = "h3h035Eyz5A"  # Sia - Unstoppable (Lyrics)
-print(f"Watch time: {lib.WatchTime(videoID)}")
+video_id = b"h3h035Eyz5A"  # Sia - Unstoppable (Lyrics)
+print(f"Watch time: {lib.WatchTime(video_id)}")
