9805009b96a4b5bbcec1387116e76eafb0cb4054
Ilya Mateyko <me@astrophena.name>
2021-11-26 20:00:32 +0300

    astrobug: add home page

--- astrobug/astrobug/urls.py
+++ astrobug/astrobug/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 
+from . import views
+
 urlpatterns = [
+    path("", views.home, name="home"),
     path("admin/", admin.site.urls),
     path("bugs/", include("bugs.urls")),
     path("accounts/", include("django.contrib.auth.urls")),
--- astrobug/bugs/views.py
+++ astrobug/bugs/views.py
@@ -1,7 +1,7 @@
 from django.views import generic
+from django.contrib.auth.decorators import login_required
 
 from .models import Bug
-
 
 class IndexView(generic.ListView):
     template_name = "bugs/index.html"
@@ -10,7 +10,6 @@ class IndexView(generic.ListView):
     def get_queryset(self):
         """Return all bugs."""
         return Bug.objects.order_by("created_at")
-
 
 class DetailView(generic.DetailView):
     model = Bug
