631f9ecdd6afb4d7c1c6c2cafca149540c5bda82
Ilya Mateyko <me@astrophena.name>
2021-12-16 21:47:36 +0300

    cmd/sqlplay: use go 1.18

--- cmd/sqlplay/deploy.sh
+++ cmd/sqlplay/deploy.sh
@@ -5,16 +5,12 @@ set -euo pipefail
 # Keep these variables in sync with https://git.astrophena.name/infra/tree/build/build.go#n83.
 
 pkg="git.astrophena.name/infra/version"
-commit="$(git rev-parse HEAD)"
-branch="$(git rev-parse --abbrev-ref HEAD)"
 env="prod"
-date="$(date)"
-builtBy="$USER@$HOSTNAME"
 
 # See https://git.astrophena.name/infra/tree/internal/maint/build?id=0233c70f8251093d73d4534e8cddda695dec4e33 for how quoting works.
-ldflags="-s -w -buildid="" -X $pkg.Commit="$commit" -X '$pkg.Branch=$branch' -X '$pkg.Env=$env' -X '$pkg.Date=$date' -X '$pkg.BuiltBy=$builtBy'"
+ldflags="-s -w -buildid="" -X '$pkg.Env=$env'"
 
 cd "$(dirname $0)"
-CGO_ENABLED=0 go build -ldflags="$ldflags" -tags="$env" -trimpath
+CGO_ENABLED=0 go build -ldflags="$ldflags" -trimpath
 rsync -aP sqlplay infra:/home/astrophena/.local/bin
 ssh infra systemctl --user restart sqlplay
