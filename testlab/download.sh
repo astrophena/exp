624e869984af25537d7720d6fc38eab3da695694
Ilya Mateyko <me@astrophena.name>
2022-01-27 16:24:04 +0300

    testlab: fix unbound variable in download.sh

--- testlab/download.sh
+++ testlab/download.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 
 image_url="http://cloud.debian.org/images/cloud/bullseye/latest/debian-11-generic-amd64.qcow2"
 name="debian"
-[[ ! -z "$1" ]] && name="$1"
+[[ ! -z "${1:-}" ]] && name="$1"
 
 [[ -f "$name.qcow2" ]] && {
 	echo "$name.qcow2 does already exist."
