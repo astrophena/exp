b0089891ed604dbca6744f879519425571b24fc9
Ilya Mateyko <me@astrophena.name>
2022-06-03 15:09:15 +0300

    doc.go: update

--- doc.go
+++ doc.go
@@ -1,3 +1,6 @@
-// Exp contains various experiments. Use them with discretion and have fun!
-// Anything here can disappear at any moment.
+/*
+Exp holds experimental code and things that I play with. Use them
+with discretion and have fun! Beware that anything here can disappear at any
+moment.
+*/
 package exp
